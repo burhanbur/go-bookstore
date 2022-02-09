e91f0f7b0a95ed2b3aecd056456ed4c38be284c0
Burhan Mafazi <burhanburdev@gmail.com>
2022-02-09 17:37:02 +0700

    complete CRUD

--- controllers/books.go
+++ controllers/books.go
@@ -21,6 +21,18 @@ func FindBooks(c *gin.Context) {
 	)
 }
 
+func FindBook(c *gin.Context) {
+	var book models.Book
+
+	if err := config.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
 func CreateBook(c *gin.Context) {
 	var input models.CreateBookInput
 	err := c.ShouldBindJSON(&input)
@@ -40,4 +52,37 @@ func CreateBook(c *gin.Context) {
 			"data": book,
 		},
 	)
+}
+
+func UpdateBook(c *gin.Context) {
+	var book models.Book
+
+	if err := config.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+
+		return
+	}
+
+	var input models.UpdateBookInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	config.DB.Model(&book).Updates(input)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func DeleteBook(c *gin.Context) {
+	var book models.Book
+
+	if err := config.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	config.DB.Delete(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": true})
 }
--- main.go
+++ main.go
@@ -16,7 +16,10 @@ func main() {
 	config.InitDb()
 
 	router.GET("/books", controllers.FindBooks)
+	router.GET("/books/:id", controllers.FindBook)
 	router.POST("/books", controllers.CreateBook)
+	router.PATCH("/books/:id", controllers.UpdateBook)
+	router.DELETE("/books/:id", controllers.DeleteBook)
 
 	fmt.Println("CONNECTED TO PORT 8080")
 	log.Fatal(router.Run(":8080"))
